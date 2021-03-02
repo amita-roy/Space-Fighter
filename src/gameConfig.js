9de310173bc8836c494cec24fe31bb9064ffb366
Amita-Roy <sardana.amita2@gmail.com>
2021-03-02 11:23:52 +0100

    Add config for physics and scenes

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -2,15 +2,35 @@ import Phaser from 'phaser';
 
 const WIDTH = 1200;
 const HEIGHT = 800;
+const PLAYER_POSITION = {
+  x: WIDTH / 2,
+  y: HEIGHT,
+};
 
 const SHARED_CONFIG = {
   width: WIDTH,
   height: HEIGHT,
+  screenCenter: PLAYER_POSITION,
 };
+
+const scenes = [];
+const createScene = (Scene) => new Scene(SHARED_CONFIG);
+
+const initScenes = () => scenes.map(createScene);
 
 const config = {
   type: Phaser.AUTO,
   ...SHARED_CONFIG,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: false,
+    },
+  },
+  scene: initScenes(),
+  pixelArt: true,
+  roundPixels: true,
 };
 
 export default config;
