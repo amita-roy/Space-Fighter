40c12e6e0878f5d6fb8ee09deea724d2632fb6c2
Amita-Roy <sardana.amita2@gmail.com>
2021-03-02 09:49:36 +0100

    Initial project setup with linters, webpack and jest

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.wav$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+};
